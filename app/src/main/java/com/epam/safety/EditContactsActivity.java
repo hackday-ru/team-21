ecfca4e7fc78741054ffe5b7feaac32ad9c47ade
dbychkov <mail@dbychkov.com>
2016-04-16 16:37:03 +0300

    Fab contacts screen

--- app/src/main/java/com/epam/safety/EditContactsActivity.java
+++ app/src/main/java/com/epam/safety/EditContactsActivity.java
@@ -1,6 +1,9 @@
 package com.epam.safety;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.view.View;
+import android.widget.Toast;
 
 public class EditContactsActivity extends BaseActivity {
 
@@ -12,6 +15,19 @@ public class EditContactsActivity extends BaseActivity {
 
     @Override
     void onCreateLayout(Bundle savedInstanceState) {
+        setupFab();
+    }
+
+    public void setupFab() {
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(EditContactsActivity.this, "Contacts", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
+
+
 }
